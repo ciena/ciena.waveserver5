478ddecfc0443f44902e459b867dc1bbf12a484c
Jeff Groom <jgroom@ciena.com>
2023-05-31 13:21:30 -0600

    facts yo

---
+++ plugins/module_utils/network/waveserver5/facts/legacy/base.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The SAOS 10 Legacy fact class
+It is in this file the configuration is collected from the device
+for a given resource, parsed, and the facts tree is populated
+based on the configuration.
+"""
+
+from __future__ import absolute_import, division, print_function
+from ansible.module_utils._text import to_text
+
+__metaclass__ = type
+import platform
+from ansible_collections.ciena.waveserver5.plugins.module_utils.network.waveserver5.waveserver5 import (
+    get_configuration,
+    get_capabilities,
+    remove_ns,
+)
+
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.netconf.netconf import (
+    get,
+)
+
+try:
+    from lxml.etree import tostring as xml_to_string
+    from lxml import etree
+
+    HAS_LXML = True
+except ImportError:
+    from xml.etree.ElementTree import tostring as xml_to_string
+
+    HAS_LXML = False
+
+
+class FactsBase(object):
+
+    COMMANDS = frozenset()
+
+    def __init__(self, module):
+        self.module = module
+        self.facts = dict()
+        self.warnings = list()
+        self.responses = None
+
+    def tostring(element, encoding="UTF-8"):
+        if HAS_LXML:
+            return xml_to_string(element, encoding="unicode")
+        else:
+            return to_text(xml_to_string(element, encoding), encoding=encoding)
+
+    def populate(self):
+        raise NotImplementedError
+
+
+class Default(FactsBase):
+    def populate(self):
+        self.facts.update(self.platform_facts())
+        # Get some properties from the <components> GET
+        config_filter = '<components xmlns="http://openconfig.net/yang/platform"/>'
+        reply = get(self.module, filter=("subtree", config_filter))
+        root = remove_ns(reply)
+        serial_number = root.xpath("//component[name='Waveserver']/state/serial-no")[0].text
+        platform = root.xpath("//component[name='Waveserver']/state/id")[0].text
+        model = root.xpath("//component[name='Waveserver']/state/description")[0].text
+        network_os_version = root.xpath("//component[name='CM-1']/state/software-version")[0].text
+
+        self.facts["serialnum"] = serial_number
+        self.facts["model"] = model
+        self.facts["platform"] = platform
+        self.facts["version"] = network_os_version
+
+        # Get some properties from the <system> GET
+        config_filter = '<system xmlns="http://openconfig.net/yang/system"><config><hostname/></config></system>'
+        reply = get(self.module, filter=("subtree", config_filter))
+        root = remove_ns(reply)
+        hostname = root.xpath("/data/system/config/hostname")[0].text
+        self.facts["hostname"] = hostname
+
+    def platform_facts(self):
+        platform_facts = {}
+
+        resp = get_capabilities(self.module)
+        device_info = resp["device_info"]
+
+        platform_facts["system"] = device_info["network_os"]
+
+        for item in ("image", "version", "hostname"):
+            val = device_info.get("network_os_%s" % item)
+            if val:
+                platform_facts[item] = val
+
+        platform_facts["api"] = resp["network_api"]
+        platform_facts["python_version"] = platform.python_version()
+
+        return platform_facts
+
+
+class Config(FactsBase):
+    def populate(self):
+        config_format = self.module.params["config_format"]
+        config_format = "xml"
+        reply = get_configuration(self.module, format="xml")
+
+        if config_format == "xml":
+            config = xml_to_string(remove_ns(reply))
+
+        elif config_format == "text":
+            raise Exception("text Not yet Implemented")
+
+        elif config_format == "json":
+            raise Exception("json Not yet Implemented")
+
+        self.facts["config"] = config
---
+++ plugins/modules/waveserver5_facts.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2019 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+"""
+The module file for waveserver5_facts
+"""
+
+
+DOCUMENTATION = """
+module: waveserver5_facts
+short_description: Get facts about waveserver5 devices.
+description:
+- Collects facts from network devices running the waveserver5 operating system. This module
+  places the facts gathered in the fact tree keyed by the respective resource name.  The
+  facts module will always collect a base set of facts from the device and can enable
+  or disable collection of additional facts.
+version_added: 1.0.0
+author:
+- Jeff Groom
+notes:
+- Tested against SAOS 10-4.
+options:
+  gather_subset:
+    description:
+    - When supplied, this argument will restrict the facts collected to a given subset.  Possible
+      values for this argument include all, default, config, and neighbors. Can specify
+      a list of values to include a larger subset. Values can also be used with an
+      initial C(M(!)) to specify that a specific subset should not be collected.
+    required: false
+    default: '!config'
+  gather_network_resources:
+    description:
+      - When supplied, this argument will restrict the facts collected
+        to a given subset. Possible values for this argument include
+        all and the resources like interfaces, vlans etc.
+        Can specify a list of values to include a larger subset. Values
+        can also be used with an initial C(M(!)) to specify that a
+        specific subset should not be collected.
+    required: false
+"""
+
+EXAMPLES = """
+# Gather all facts
+- waveserver5_facts:
+    gather_subset: all
+    gather_network_resources: all
+
+"""
+
+RETURN = """
+ansible_net_config:
+  description: The running-config from the device
+  returned: when config is configured
+  type: str
+ansible_net_model:
+  description: The device model string
+  returned: always
+  type: str
+ansible_net_serialnum:
+  description: The serial number of the device
+  returned: always
+  type: str
+ansible_net_version:
+  description: The version of the software running
+  returned: always
+  type: str
+ansible_net_neighbors:
+  description: The set of LLDP neighbors
+  returned: when interface is configured
+  type: list
+ansible_net_gather_subset:
+  description: The list of subsets gathered by the module
+  returned: always
+  type: list
+ansible_net_api:
+  description: The name of the transport
+  returned: always
+  type: str
+ansible_net_python_version:
+  description: The Python version Ansible controller is using
+  returned: always
+  type: str
+ansible_net_gather_network_resources:
+  description: The list of fact resource subsets collected from the device
+  returned: always
+  type: list
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.ciena.waveserver5.plugins.module_utils.network.waveserver5.argspec.facts.facts import (
+    FactsArgs,
+)
+from ansible_collections.ciena.waveserver5.plugins.module_utils.network.waveserver5.facts.facts import (
+    Facts,
+)
+
+
+def main():
+    """
+    Main entry point for module execution
+
+    :returns: ansible_facts
+    """
+
+    module = AnsibleModule(
+        argument_spec=FactsArgs.argument_spec, supports_check_mode=True
+    )
+
+    warnings = []
+    if module.params["gather_subset"] == "!config":
+        warnings.append(
+            "default value for `gather_subset` will be changed to `min` from `!config` v2.11 onwards"
+        )
+
+    result = Facts(module).get_facts()
+
+    ansible_facts, additional_warnings = result
+    warnings.extend(additional_warnings)
+
+    module.exit_json(ansible_facts=ansible_facts, warnings=warnings)
+
+
+if __name__ == "__main__":
+    main()
