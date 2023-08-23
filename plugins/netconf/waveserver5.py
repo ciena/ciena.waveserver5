56c03f85707bd2ee5caf69e9bcbc4e3a7037e092
Jeff Groom <jgroom@ciena.com>
2023-08-23 09:35:17 -0600

    simpler netconf plugin

--- plugins/netconf/waveserver5.py
+++ plugins/netconf/waveserver5.py
@@ -40,11 +40,8 @@ options:
 """
 
 import json
-import re
 
 
-from ansible.errors import AnsibleConnectionFailure
-from ansible.module_utils._text import to_native
 from ansible_collections.ansible.netcommon.plugins.plugin_utils.netconf_base import (
     NetconfBase,
     ensure_ncclient,
@@ -55,10 +52,8 @@ from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.n
 
 
 try:
-    from ncclient import manager
     from ncclient.operations import RPCError
-    from ncclient.transport.errors import SSHUnknownHostError
-    from ncclient.xml_ import new_ele, sub_ele, to_ele, to_xml
+    from ncclient.xml_ import to_ele, to_xml
 
     HAS_NCCLIENT = True
 except (
@@ -133,82 +128,3 @@ class Netconf(NetconfBase):
         """Extract data using XPath and return text or default."""
         result = root.xpath(xpath_str)
         return result[0].text if result else default
-
-    @staticmethod
-    @ensure_ncclient
-    def guess_network_os(obj):
-        """
-        Guess the remote network os name
-        :param obj: Netconf connection class object
-        :return: Network OS name
-        """
-        try:
-            m = manager.connect(
-                host=obj._play_context.remote_addr,
-                port=obj._play_context.port or 830,
-                username=obj._play_context.remote_user,
-                password=obj._play_context.password,
-                key_filename=obj.key_filename,
-                hostkey_verify=obj.get_option("host_key_checking"),
-                look_for_keys=obj.get_option("look_for_keys"),
-                allow_agent=obj._play_context.allow_agent,
-                timeout=obj.get_option("persistent_connect_timeout"),
-                # We need to pass in the path to the ssh_config file when guessing
-                # the network_os so that a jumphost is correctly used if defined
-                ssh_config=obj._ssh_config,
-            )
-        except SSHUnknownHostError as exc:
-            raise AnsibleConnectionFailure(to_native(exc))
-
-        guessed_os = None
-        for c in m.server_capabilities:
-            # TODO HACK
-            raise AnsibleConnectionFailure("Unable to guess network os")
-            if re.search("default", c):
-                guessed_os = "waveserver5"
-
-        m.close_session()
-        return guessed_os
-
-    def execute_rpc(self, name):
-        """
-        RPC to be execute on remote device
-        :param name: Name of rpc in string format
-        :return: Received rpc response from remote host
-        """
-        return self.rpc(name)
-
-    @ensure_ncclient
-    def edit_config(
-        self,
-        config=None,
-        format="xml",
-        target="candidate",
-        default_operation=None,
-        test_option=None,
-        error_option=None,
-    ):
-        """
-        Loads all or part of the specified *config* to the *target* configuration datastore.
-        :param config: Is the configuration, which must be rooted in the `config` element.
-                       It can be specified either as a string or an :class:`~xml.etree.ElementTree.Element`.
-        :param format: The format of configuration eg. xml, text
-        :param target: Is the name of the configuration datastore being edited
-        :param default_operation: If specified must be one of { `"merge"`, `"replace"`, or `"none"` }
-        :param test_option: If specified must be one of { `"test_then_set"`, `"set"` }
-        :param error_option: If specified must be one of { `"stop-on-error"`, `"continue-on-error"`, `"rollback-on-error"` }
-                             The `"rollback-on-error"` *error_option* depends on the `:rollback-on-error` capability.
-        :return: Returns xml string containing the RPC response received from remote host
-        """
-        if config is None:
-            raise ValueError("config value must be provided")
-        resp = self.m.edit_config(
-            config,
-            format=format,
-            target=target,
-            default_operation=default_operation,
-            test_option=test_option,
-            error_option=error_option,
-        )
-
-        return resp.data_xml if hasattr(resp, "data_xml") else resp.xml
