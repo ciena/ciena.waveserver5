4e57368ffad1256c3ce07c24c39dcc91390f509e
Jeff Groom <jgroom@ciena.com>
2023-09-22 14:43:28 -0600

    lint fixes

--- plugins/module_utils/network/waveserver5/facts/system/system.py
+++ plugins/module_utils/network/waveserver5/facts/system/system.py
@@ -19,7 +19,6 @@ from copy import deepcopy
 
 import re  # pylint: disable=unused-import
 from ansible.module_utils._text import to_bytes
-from ansible.module_utils.basic import missing_required_lib
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.netconf import (
     remove_namespaces,
 )
